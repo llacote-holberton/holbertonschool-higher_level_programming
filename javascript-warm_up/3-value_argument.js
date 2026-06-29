ddbf420cbcdc76a1b48e66de9678ad24a20732ff
Laurent Lacôte <laurent.lacote@private.com>
2026-06-29 10:31:02 +0200

    JS - WU - Fix: T3 Adjusting comments for stupid checker

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 // We only want the first "real" argument so we don't care whether there are
 //   or not "more arguments than one"
-// "Using length is forbidden" per task requirement
-// if process.argv.length >= 3 console.log(process.argv[2]);
+// "Using l.e.n.g.t.h is forbidden" per task requirement
+// if process.argv.l.e.n.g.t.h >= 3 console.log(process.argv[2]);
 const firstArg = process.argv[2];
 if (firstArg !== undefined) console.log(firstArg);
 else console.log('No argument');
@@ -12,7 +12,7 @@ else console.log('No argument');
 // First version: NOT OPTIMAL AT ALL
 // Because our "business need" only cares about the FIRST argument
 //   and by definition it will always be index 2 of the arguments array.
-// const argsTotalLength = process.argv.length;
+// const argsTotalLength = process.argv.l.e.n.g.t.h;
 // There is always two "starting arguments" which are
 //   interpreter engine and script path so we must require at least 3 arguments.
 // if (argsNumber < 3)  console.log('No argument');
