27a27c6fe4338e75323e2b276d0bf5ad5357d35b
Laurent Lacôte <laurent.lacote@private.com>
2026-06-29 10:42:42 +0200

    JS - WU - Feat: T6 Simple array looping

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Must use a loop and a single print.
+const myLines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+myLines.forEach(element => console.log(element));
